4720f940fc9e0064253b130e7af728c76e990e20
AhshanHabib26 <habib.net27@gmail.com>
2023-06-12 12:56:15 +0600

    User Model Update

--- models/userModel.js
+++ models/userModel.js
@@ -20,6 +20,20 @@ const userSchema = mongoose.Schema(
       required: [true, "Please add a password"],
       minlength: 6,
     },
+    photo: {
+      type: String,
+      required: [true, "Please add a photo"],
+      default: "avatar.jpg",
+    },
+    phone: {
+      type: String,
+      default: "+8801XXXXXXX",
+    },
+    bio: {
+      type: String,
+      default: "Enter Your Details",
+      maxLength: [250, "Bio must not be more than 250 characters"],
+    },
     role: {
       type: String,
       enum: ["user", "admin"],
